03d27132e25c7f6fef7db5521276ec18ee4392e0
gipsy martinez torrealba <gipsy martinez torrealba>
2017-02-18 02:11:27 -0400

    commit inicial

---
+++ js/javascript.js
@@ -0,0 +1,6 @@
+ $(function(){
+ 	$(".type-on").typed({
+ 		strings: ["a Learner", "a Web Designer", "a Web Developer", "a Web Enthusiast", "a Software Engineer", "a Venezuelan Woman", "Guara!", "a Beatles lover", "a Pokemon Master",  "an alligator XD (8)", "a mama-papa coming for you (8)", "the space invader (8) haha", "Gipsy Martínez :)"],
+ 		typeSpeed: 0
+ 	});
+ });
