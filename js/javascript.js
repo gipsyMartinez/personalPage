2616052ceb3df0582e3a44d445a2aed213359e3c
gipsy martinez torrealba <gipsy.martinez@gmail.com>
2017-02-19 14:48:46 -0400

    - Fixed navbar - aniview efects

--- js/javascript.js
+++ js/javascript.js
@@ -4,3 +4,14 @@
  		typeSpeed: 0
  	});
  });
+ 
+$(document).ready(function() {
+    var options = {
+        animateThreshold: 100
+    };
+    $('.aniview').AniView(options);
+
+    $('.code').each(function(i, block) {
+        hljs.highlightBlock(block);
+    });
+});
