23b6061ff9a3d14d9bbcf19cd2758c1bdd62cd05
gipsy martinez torrealba <gipsy martinez torrealba>
2017-02-19 11:08:28 -0400

    Github Pages test

--- js/javascript.js
+++ js/javascript.js
@@ -1,6 +1,6 @@
  $(function(){
  	$(".type-on").typed({
- 		strings: ["a Learner", "a Web Designer", "a Web Developer", "a Web Enthusiast", "a Software Engineer", "a Venezuelan Woman", "Guara!", "a Beatles lover", "a Pokemon Master",  "an alligator XD (8)", "a mama-papa coming for you (8)", "the space invader (8) haha", "Gipsy Martínez :)"],
+ 		strings: ["a Learner", "a Web Designer", "a Web Developer", "a Web Enthusiast", "a Software Engineer", "a Venezuelan Woman", "Guara!", "a Beatles lover", "a Pokemon Master",  "an alligator XD (8)", "a mama-papa coming for you (8)", "the space invader (8) haha", "Test github pages" ,"Gipsy Martínez :)"],
  		typeSpeed: 0
  	});
  });
